5c8845531ae7928434912037a936896f74b73f6d
mynung <alsghd72@gmail.com>
2026-04-23 13:39:08 +0900

    체크박스 추가 및 디자인 변경

--- script.js
+++ script.js
@@ -2,21 +2,39 @@ const input = document.getElementById("todo-input");
 const addButton = document.getElementById("add-btn");
 const todoList = document.getElementById("todo-list");
 
-addButton.addEventListener("click", () => {
+const event = () => {
     const inputValue = input.value;
     if (inputValue.trim() !== "") {
         const li = document.createElement("li");
-        const text = document.createElement("span");
+        const text = document.createElement("span"); //텍스트 요소 생성
+        const remove_button = document.createElement("button"); //삭제 버튼 생성
+        const checkbox = document.createElement("input"); //체크박스 생성;
+        checkbox.setAttribute("type", "checkbox");
+        checkbox.addEventListener("change", () => {
+            if (checkbox.checked) {
+                text.style.textDecoration = "line-through";
+                   }
+                   else{
+                    text.style.textDecoration = "none";
+                   }
+        });
         text.textContent = inputValue;
-        const remove_button = document.createElement("button");
         remove_button.setAttribute( "id", "r_btn" )
         remove_button.textContent = "X";
         remove_button.addEventListener("click", () => {
             todoList.removeChild(li);
         });
+        li.appendChild(checkbox);
         li.appendChild(text);
         li.appendChild(remove_button);
+
         todoList.appendChild(li);
         input.value = "";
+    }
+}
+addButton.addEventListener("click", event);
+input.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        event();
     }
 });
